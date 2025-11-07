0e17dc779b1885dddc1c55b36235016fc59880a2
Angelo <martinezglielmo@gmail.com>
2025-11-07 16:37:58 +0000

    encrypted passwords using bcrypt

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,8 @@
+
 import sequelize from "../controllers/dbController.js";
 import { DataTypes } from "sequelize";
+import bcrypt from "bcrypt";
+
 
 const User = sequelize.define(
     "User",
@@ -24,6 +27,20 @@ const User = sequelize.define(
     },
     {
         timestamps: true,
+        hooks: {
+            beforeCreate: async (user) => {
+                if (user.password) {
+                    const salt = await bcrypt.genSalt(10);
+                    user.password = await bcrypt.hash(user.password, salt);
+                }
+            },
+            beforeUpdate: async (user) => {
+                if (user.changed("password")) {
+                    const salt = await bcrypt.genSalt(10);
+                    user.password = await bcrypt.hash(user.password, salt);
+                }
+            },
+        },
     }
 );
 
