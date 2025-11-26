1c271f302b75195334ec5c4d56e66159d9af8b02
angelo <martinezglielmo@gmail.com>
2025-11-26 18:21:09 +0100

    fix: update CORS origin to use HTTPS for canaryweather.xyz

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,17 @@ export default defineConfig({
     plugins: [react(), tailwind()],
     server: {
         allowedHosts: ['canaryweather.xyz'],
+        proxy: {
+            '/api': {
+                target: 'http://localhost:85',
+                changeOrigin: true,
+                secure: false,
+            },
+            '/uploads': {
+                target: 'http://localhost:85',
+                changeOrigin: true,
+                secure: false,
+            },
+        }
     }
 });
